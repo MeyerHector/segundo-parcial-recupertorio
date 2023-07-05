1c1a22591a4cf26d69a01a785c20a739e6e7d74e
Meyer <meyer-hector@hotmail.com>
2023-07-05 14:23:59 -0300

    Se configuro app.js, conexion a la base de datos y modelo

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 // Imports
 const cors = require('cors');
 const express = require('express');
-
+const morgan = require('morgan');
 const path = require('path');
+require('ejs');
+require('dotenv').config();
+port = process.env.PORT || 4500;
+
+const { conexionDB } = require('./db')
+conexionDB()
 
 const app = express();
 
 // Middlewares
 // TODO: Implementar middlewares
-
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Routes
 app.use('/api', require('./routes/reserva.routes'));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
+app.use((req, res)=> {
+    res.status(404).send('<h1>ERROR 404, PAGINA NO ENCONTRADA</h1>');
+})
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+
+app.listen(port, () => console.log(`Server on http://localhost:${port}`));
--- models/Reserva.js
+++ models/Reserva.js
@@ -1,1 +1,64 @@
 // TODO: Crear modelo de datos de Reserva
+const { sequelize, DataTypes } = require('../db')
+
+const Reserva = sequelize.define('Reserva', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    codigo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        defaultValue: new Date().getTime()
+    },
+    nombre: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    apellido: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    dni: {
+        type: DataTypes.STRING(11),
+        allowNull: false
+    },
+    fechaIda: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+    },
+    fechaVuelta: {
+        type: DataTypes.DATEONLY,
+        allowNull: true
+    },
+    estado: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }
+}, {
+    createdAt: true,
+    updatedAt: true,
+    deletedAt: true,
+    tableName: 'Reserva'
+})
+
+Reserva.sync({ force: false }).then(() => {
+    console.log('Tabla de Reservas creada');
+});
+module.exports = Reserva
